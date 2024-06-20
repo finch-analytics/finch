0c2f2eae84174ef1cb7abd8bd39547ff22e942a0
Jan Philipp Tebernum <jan.tebernum@pm.me>
2024-06-20 23:38:00 +0200

    fix: use a test submodule for lib

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,12 @@ pub fn adder(a: i32, b: i32) -> i32 {
     a + b
 }
 
-#[test]
-fn simple_test() {
-    assert_eq!(adder(2, 2), 4);
+#[cfg(test)]
+mod tests {
+    use super::adder;
+
+    #[test]
+    fn simple_test() {
+        assert_eq!(adder(2, 2), 4);
+    }
 }
