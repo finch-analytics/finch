5286eb45e4199cf3db20b7c84842defffc622f93
Jan Philipp Tebernum <jan.tebernum@pm.me>
2024-06-25 00:41:14 +0200

    feat: basic tonic client example

--- src/client.rs
+++ src/client.rs
@@ -1,1 +1,18 @@
-fn main() {}
+use hello_world::{greeter_client::GreeterClient, HelloRequest};
+
+pub mod hello_world {
+    tonic::include_proto!("helloworld");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = GreeterClient::connect("http://[::1]:50051").await?;
+
+    let request = tonic::Request::new(HelloRequest { name: "Jan".into() });
+
+    let response = client.say_hello(request).await?;
+
+    println!("RESPONSE={:?}", response);
+
+    Ok(())
+}
