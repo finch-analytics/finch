c675bb05c1a78993a253a4b820afa6791cedf8a5
Jan Philipp Tebernum <jan.tebernum@pm.me>
2024-07-29 22:50:15 +0200

    chore: reworked project structure

--- crates.nix
+++ crates.nix
@@ -1,14 +1,29 @@
-{ ... }: {
-  perSystem = { pkgs, ... }:
-    let crateName = "finch";
-    in {
+{ ... }:
+{
+  perSystem =
+    { pkgs, ... }:
+    let
+      projectName = "finch";
+    in
+    {
       # use toolchain file
       nci.toolchainConfig = ./rust-toolchain.toml;
+
       # declare projects
-      nci.projects."simple".path = ./.;
-      # configure crates
-      nci.crates.${crateName} = {
-        drvConfig = { mkDerivation = { buildInputs = [ pkgs.protobuf_27 ]; }; };
+      nci.projects.${projectName} = {
+        path = ./.;
+        export = true;
       };
+
+      # configure crates
+      nci.crates."finch-root" = {
+        drvConfig = {
+          mkDerivation = {
+            buildInputs = [ pkgs.protobuf_27 ];
+          };
+        };
+      };
+
+      nci.crates."spark-submit" = { };
     };
 }
--- flake.nix
+++ flake.nix
@@ -10,13 +10,10 @@
 
     treefmt-nix.url = "github:numtide/treefmt-nix";
     pre-commit-hooks-nix.url = "github:cachix/pre-commit-hooks.nix";
-    # crane = {
-    #   url = "github:ipetkov/crane";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
   };
 
-  outputs = inputs@{ parts, nci, ... }:
+  outputs =
+    inputs@{ parts, nci, ... }:
     parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" ];
 
@@ -27,12 +24,14 @@
         ./crates.nix
       ];
 
-      perSystem = { pkgs, config, ... }:
+      perSystem =
+        { pkgs, config, ... }:
         let
           # shorthand for accessing this crate's outputs
           # you can access crate outputs under `config.nci.outputs.<crate name>` (see documentation)
-          crateOutputs = config.nci.outputs."finch";
-        in {
+          outputs = config.nci.outputs;
+        in
+        {
           treefmt = {
             projectRootFile = "flake.nix";
             programs.nixfmt.enable = true;
@@ -45,26 +44,32 @@
           pre-commit = {
             check.enable = false;
             settings = {
-              hooks.nixfmt.enable = true;
+              # hooks.nixfmt.enable = true;
               hooks.rustfmt.enable = true;
-              # hooks.clippy.enable = true;
               hooks.cargo-check.enable = true;
               hooks.deadnix.enable = true;
             };
           };
 
           # export the crate devshell as the default devshell
-          devShells.default = crateOutputs.devShell.overrideAttrs (old: {
-            packages = with pkgs;
+          devShells.default = outputs."finch".devShell.overrideAttrs (old: {
+            packages =
+              with pkgs;
               (old.packages or [ ])
-              ++ [ bacon cargo-nextest cargo-tarpaulin grpcurl protobuf_27 ];
+              ++ [
+                bacon
+                cargo-nextest
+                cargo-tarpaulin
+                grpcurl
+                protobuf_27
+              ];
             shellHook = ''
               ${old.shellHook or ""}
               ${config.pre-commit.installationScript}
             '';
           });
           # export the release package of the crate as default package
-          packages.default = crateOutputs.packages.release;
+          packages.default = outputs."finch-root".packages.release;
         };
     };
 }
