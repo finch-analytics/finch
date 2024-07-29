10983e627d0911297d61f01e525fe38dc3d54c67
Jan Philipp Tebernum <jan.tebernum@pm.me>
2024-07-29 02:05:01 +0200

    feat: some basic authorization

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,5 @@
 use hello_world::{greeter_client::GreeterClient, HelloRequest};
+use tonic::{metadata::MetadataValue, transport::Channel, Request};
 
 pub mod hello_world {
     tonic::include_proto!("helloworld");
@@ -6,7 +7,14 @@ pub mod hello_world {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = GreeterClient::connect("http://[::1]:50051").await?;
+    let channel = Channel::from_static("http://[::1]:50051").connect().await?;
+
+    let token: MetadataValue<_> = "Bearer some-secret-token".parse()?;
+
+    let mut client = GreeterClient::with_interceptor(channel, move |mut req: Request<()>| {
+        req.metadata_mut().insert("authorization", token.clone());
+        Ok(req)
+    });
 
     let request = tonic::Request::new(HelloRequest { name: "Jan".into() });
 
--- src/server.rs
+++ src/server.rs
@@ -1,8 +1,5 @@
-use hello_world::{
-    greeter_server::{Greeter, GreeterServer},
-    HelloReply, HelloRequest,
-};
-use tonic::{transport::Server, Request, Response, Status};
+use hello_world::{greeter_server::Greeter, HelloReply, HelloRequest};
+use tonic::{metadata::MetadataValue, transport::Server, Request, Response, Status};
 
 pub mod hello_world {
     tonic::include_proto!("helloworld");
@@ -32,10 +29,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
     let greeter = MyGreeter::default();
 
-    Server::builder()
-        .add_service(GreeterServer::new(greeter))
-        .serve(addr)
-        .await?;
+    let svc = hello_world::greeter_server::GreeterServer::with_interceptor(greeter, check_auth);
+
+    Server::builder().add_service(svc).serve(addr).await?;
 
     Ok(())
+}
+
+fn check_auth(req: Request<()>) -> Result<Request<()>, Status> {
+    let token: MetadataValue<_> = "Bearer some-secret-token".parse().unwrap();
+
+    match req.metadata().get("authorization") {
+        Some(t) if token == t => Ok(req),
+        _ => {
+            eprintln!("unauthorized access attempt");
+            Err(Status::unauthenticated("No valid auth token"))
+        }
+    }
 }
